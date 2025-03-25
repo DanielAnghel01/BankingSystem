b1a6fed0da74d0ff7b5618af60f13721b5b80416
Daniel <angheldaniel674@gmail.com>
2025-03-25 20:09:13 +0200

    Added Swagger and modified README.MD file

--- BankingSystem/BankingSystem/Program.cs
+++ BankingSystem/BankingSystem/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "BankingSystem", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -12,12 +16,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BankingSystem v1"));
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Test
+# BankingSystem
+This is an ASP.NET Web API project for a Banking System. 
+It provides essential banking functionalities such as account management, transactions, and user authentication. 
+Built with C# and .NET, it ensures security and efficiency for handling banking operations.
